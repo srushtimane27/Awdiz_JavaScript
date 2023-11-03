6dfe4c70752bb26649416d4a15160f680dba504d
srushtimane27 <srushtimane31@gmail.com>
2023-11-03 18:28:47 +0530

    Day-5
    Shift and Unshift

---
+++ Day-5/arraytwo.js
@@ -0,0 +1,13 @@
+var characters = ["a","b","c","d","e"]
+console.log(characters)
+
+// shift
+characters.shift();
+characters.shift();
+console.log(characters)
+
+// Unshift
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+numbers.unshift(20,30)
+console.log(numbers)
+
