20a44345fb90b50d36b790c160befe948299657c
srushtimane27 <srushtimane31@gmail.com>
2023-11-03 18:30:35 +0530

    Day-6
    Reverse and Array Loop

---
+++ Day-6/loopArray.js
@@ -0,0 +1,41 @@
+// Reverse For Loop
+for(var i=10; i>0; i--){
+    console.log(i)
+}
+
+// for(var i=800; i>=400; i-=5){
+//     console.log(i)
+// }
+
+for(var i=20; i>=10; i-=2){
+    console.log(i)
+}
+
+for(var i=20; i>0; i--){
+    console.log(i)
+}
+
+for(var i=50; i>10; i-=10){
+    console.log(i)
+}
+
+// Array Loop
+var emp = ["Sahil","Akshay","Malhar","Aaryan"]
+for(var i=0; i<emp.length; i++){
+    console.log(emp[i],i)
+}
+
+var id = [1,2,3,4,5,6,7,8,9]
+for(var i = id.length - 1; i >=0; i--){
+    console.log(id[i])
+}
+
+var num = [10,20,30,40,50,60,70,80]
+for(var i = num.length - 2; i >= 0; i -= 10){
+    console.log(num[i])
+}
+
+var numbers = [1,2,3,4,5,6,7,8]
+for (var i = 12; i > 1; i -= 5){
+    console.log(numbers[i])
+}
