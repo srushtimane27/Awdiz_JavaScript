a67810dc1460870735fc08e4355eb5751511d8df
srushtimane27 <srushtimane31@gmail.com>
2023-11-03 18:26:32 +0530

    Day-3
    Functions, Data Types, Operators

---
+++ Day-3/day3.js
@@ -0,0 +1,60 @@
+console.log("Srushti")
+console.log(typeof("Srushti"))
+console.log(typeof(123))
+console.log(typeof("1235"))
+console.log("123" + "123")
+
+// Functions
+function add(num1, num2){
+    return num1 + num2
+}
+add(5,5)
+
+// Data Type - Boolean
+console.log(true)
+console.log(typeof(true))
+console.log(false)
+console.log(typeof(false))
+console.log(typeof('false'))
+
+// Variables
+var name = "Srushti"
+console.log(name)
+console.log(typeof(name))
+
+var batchNumber = 10;
+console.log(batchNumber)
+console.log(typeof(batchNumber))
+
+var abc = true;
+console.log(abc)
+
+var a = 10;
+var b = 20;
+console.log(a, b)
+console.log(a + b)
+
+// Arithmatic Oprator
+console.log(20 % 2)
+console.log(23 % 2)
+console.log(2893475 % 2)
+console.log(2 == 20)
+console.log(20 % 2 == 0)
+
+// Comparison Operator
+console.log(2 == 2)
+console.log("4" == 4)
+console.log("50" === 50)
+console.log("500" === 500)
+
+
+// Logical Operator - && , ||
+
+var age = 20;
+console.log(age > 18 && age < 25)
+console.log(age < 18 || age > 80 || age < 10)
+
+console.log(6>2)
+console.log(4<8)
+console.log(4>=4)
+console.log(40<=10)
