b59810c72a787eb01f966fa1e5d8c1336a1e6037
srushtimane27 <srushtimane31@gmail.com>
2023-11-03 18:27:28 +0530

    Day-4
    Push And Pop Operations

---
+++ Day-4/array.js
@@ -0,0 +1,29 @@
+var myName
+console.log(myName)    
+console.log(typeof myName)
+
+var data = ["Srushti", 26, "Mumbai", 1234, [7,8,9]]
+console.log(data)
+
+console.log(data.length)
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+console.log(data[4])
+console.log(data[4][0])
+
+
+// Push - adds element at the end 
+var student = ["Srushti","Rasika","Ruchika"];
+console.log(student)
+student.push("Jija")
+console.log(student)
+
+// pop - removes last element
+var number = [1, 2, 3, 4]
+number.pop();
+number.pop();
+number.pop();
+console.log(number)
