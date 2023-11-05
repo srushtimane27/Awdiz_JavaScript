009db36f755355eb3fd534eea8e09d898b53fc58
srushtimane27 <srushtimane31@gmail.com>
2023-11-05 13:51:22 +0530

    Day-6
    Array JavaScript

---
+++ Day-6/praAaay.js
@@ -0,0 +1,15 @@
+// for(var i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(var i = 2; i <= 20; i++){
+//     console.log(i)
+// }
+
+// for(var i = 1; i <=20; i+=2){
+//     console.log(i)
+// }
+
+for(var i = 0; i <=20; i+=3){
+    console.log(i)
+}
