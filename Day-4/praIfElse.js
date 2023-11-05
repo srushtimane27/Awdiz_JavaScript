e20286318b79f4388d56bca9f0f74d4d25b31c3b
srushtimane27 <srushtimane31@gmail.com>
2023-11-05 13:49:59 +0530

    Day-4Practice
    Practice Program

---
+++ Day-4/praIfElse.js
@@ -0,0 +1,18 @@
+// var age = 10;
+// if(age>20){
+//     console.log("welcome")
+// }else{
+//     console.log("Bye")
+// }
+
+var num1 = 10;
+var num2 = 10;
+
+var add = num1 + num2;
+
+console.log(add)
+if (add>20) {
+    console.log("addition is more than 20")
+} else {
+    console.log("addition is less than 20")
+}
