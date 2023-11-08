58259d745746e69b0c436a33617ec6bf8084d45d
srushtimane27 <srushtimane31@gmail.com>
2023-11-08 23:08:53 +0530

    JS_Array

--- Day-7/day7.js
+++ Day-7/day7.js
@@ -1,17 +1,22 @@
+//Q.1 Print odd numbers between given range
 // for( var i=26; i<=50; i++){
 //     if(i % 2 != 0){
 //         console.log(i)
 //     }
 // }
 
+//Q.2 Print odd numbers and their count    o/p - 12
 // var count = 0;
 // for( var i=26; i<=50; i++){
 //     if(i % 2 != 0){
+//     //    console.log(i)
 //        count++
 //     }
 // }
 // console.log(count)
 
+
+//Q.3 Print odd number count and even number count
 // var oddCount = 0;
 // var evenCount = 0;
 
@@ -26,6 +31,8 @@
 // console.log(evenCount, "even count")
 
 
+//Q.4 Store odd numbers into an array
+
 // var oddNumbers = [];
 // for(var i = 25; i <= 50; i++){
 //     if(i % 2 != 0){
@@ -34,7 +41,10 @@
 // }
 // console.log(oddNumbers, 'list')
 
-// var numbers = [10,20,30,40,50,60,70,80];
+
+//Q.5 Push even numbers into an array
+
+// var numbers = [10,25,33,40,53,65,77,84];
 // var evenArray = [];
 
 // for(var i = 0; i < numbers.length; i++){
@@ -44,11 +54,11 @@
 // }
 // console.log(evenArray)
 
+//Q.6 
 
 // var array = [2, 4, 1, 8, 24, 64, 458, 1, 2, 4]
 // var target = 24;
 // var flag = false;
-
 // for(var i=0; i<array.length; i++){
 //     if(array[i] == target){
 //         console.log("Number Found")
@@ -59,13 +69,15 @@
 //     console.log("Number Not Found")
 // }
 
-var array = [1, 2, 3, 4, 5];
-var target = 5;
 
-for(var i=0; i<array.length; i++){
-    for(var j=i+1; j<array.length; j++){
-        if (array[i] + array[j] == target){
-            console.log(array[i],array[j])
-        }
-    }
-}
+// Q.7
+
+// var array = [1, 2, 3, 4, 5];
+// var target = 5;
+// for(var i=0; i<array.length; i++){
+//     for(var j=i+1; j<array.length; j++){
+//         if (array[i] + array[j] == target){
+//             console.log(array[i],array[j])
+//         }
+//     }
+// }
