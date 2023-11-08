8b99646a615c4423737ec87e03220a95e79ab73f
srushtimane27 <srushtimane31@gmail.com>
2023-11-08 23:08:13 +0530

    Practice_Array

--- Day-6/praAaay.js
+++ Day-6/praAaay.js
@@ -13,3 +13,12 @@
 for(var i = 0; i <=20; i+=3){
     console.log(i)
 }
+
+var count = 0;
+for( var i = 25; i<=50; i++){
+    if(i % 2 == 0){
+        console.log(i)
+        count++
+    }
+}
+console.log(count)
