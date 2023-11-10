83369bf12131da1ad5fb87908978acea9244a986
srushtimane27 <srushtimane31@gmail.com>
2023-11-10 16:58:16 +0530

    updates

--- Day-6/praAaay.js
+++ Day-6/praAaay.js
@@ -10,15 +10,16 @@
 //     console.log(i)
 // }
 
-for(var i = 0; i <=20; i+=3){
-    console.log(i)
-}
+// for(var i = 0; i <=20; i+=3){
+//     console.log(i)
+// }
 
-var count = 0;
-for( var i = 25; i<=50; i++){
-    if(i % 2 == 0){
-        console.log(i)
-        count++
-    }
-}
-console.log(count)
+// var count = 0;
+// for( var i = 25; i<=50; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//         count++
+//     }
+// }
+// console.log(count)
+
