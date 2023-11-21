c1f6854c61010aebd654d74698cd003bd101eedf
srushtimane27 <srushtimane31@gmail.com>
2023-11-21 15:45:41 +0530

    array
    update

--- Day-5/arraytwo.js
+++ Day-5/arraytwo.js
@@ -1,12 +1,12 @@
 var characters = ["a","b","c","d","e"]
 console.log(characters)
 
-// shift
+// shift - Delete element from start
 characters.shift();
 characters.shift();
 console.log(characters)
 
-// Unshift
+// Unshift - Add element from start
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 numbers.unshift(20,30)
 console.log(numbers)
